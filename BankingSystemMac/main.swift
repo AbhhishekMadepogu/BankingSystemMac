75d530e67daaaeed6efe6e9a7a6845a7d6cc0bfb
Richu Jain <richujain1995@gmail.com>
2019-06-11 18:10:27 -0400

    [Richu] Created Switch

--- BankingSystemMac/main.swift
+++ BankingSystemMac/main.swift
@@ -7,6 +7,25 @@
 //
 
 import Foundation
+print("Hey! How can I Help you?")
+var wantToExit = 0
+var choice : Int? = 0
+while wantToExit == 0 {
+    print("1. Create Bank Account")
+    print("2. View Account Details")
+    print("3. Create Employees")
+    print("4. Others")
+    choice = Int(readLine()!)
 
-print("Hello, World!")
-
+    switch choice! {
+        case 1 : print("Create Bank Account")
+        print("Do you want to exit(1/0)")
+        wantToExit=Int(readLine()!) ?? 0
+        break;
+        case 2 : print("View Account Details")
+        print("Do you want to exit(1/0)")
+        wantToExit=Int(readLine()!)  ?? 0
+    default:
+        print("Default")
+    }
+}
